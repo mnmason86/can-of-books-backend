f9b45e54f2f32d15c73f6655a71b81887ebf6840
Mandy Mason <mnmason86@gmail.com>
2022-07-25 21:37:29 -0400

    added books to seed.js

--- server.js
+++ server.js
@@ -3,16 +3,24 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
+const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
 
+
+mongoose.connect(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3001;
 
-app.get('/test', (request, response) => {
 
-  response.send('test request received')
 
-})
 
-app.listen(PORT, () => console.log(`listening on ${PORT}`));
+
+
+
+// app.get('/test', (request, response) => {
+
+//   response.send('test request received')
+
+// })
+
+app.listen(PORT, () => console.log(`listening on port::${PORT}`));
