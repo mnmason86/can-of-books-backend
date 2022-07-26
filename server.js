df2037de629cfdda4a2db1178525835ff4065990
Mandy Mason <mnmason86@gmail.com>
2022-07-25 21:51:00 -0400

    added getBooks and postBooks functions to server.js, removed clear.js

--- server.js
+++ server.js
@@ -6,21 +6,49 @@ const cors = require('cors');
 const mongoose = require('mongoose');
 const app = express();
 app.use(cors());
-
+app.use(express.json());
 
 mongoose.connect(process.env.DATABASE_URL);
 const PORT = process.env.PORT || 3001;
 
+const Book = require('./models/books.js');
+const db = mongoose.connection;
 
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log('Mongoose is connected');
+});
 
+app.get('/', (request, response) => {  response.status(200).send('Welcome!');})
+app.get('/books', getBooks);
+app.post('/books', postBooks);
 
+async function getBooks(req,res,next){
+  let bookQuery = {};
+  if(req.query.title){
+    bookQuery = {
+      title: req.query.title
+    }
+  }
+  try {
+    let results = await Book.find(bookQuery);
+    res.status(200).send(results);
+  }
+  catch(error){
+    next(error);
+  }
+}
+async function postBooks(req,res,next){
+  try {
+    let submittedBook = await Book.create(req.body);
+    console.log(submittedBook);
+    res.status(200).send(submittedBook);
+  }
+  catch (error){
+    next(error);
+  }
+}
 
-
-
-// app.get('/test', (request, response) => {
-
-//   response.send('test request received')
-
-// })
-
-app.listen(PORT, () => console.log(`listening on port::${PORT}`));
+app.get('*', (request, response) => {  response.status(404).send('Not available');})
+app.get('/test', (request, response) => {  response.send('test request received')})
+app.listen(PORT, () => console.log(`listening on ${PORT}`));
