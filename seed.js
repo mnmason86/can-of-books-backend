176a8a2bd349a51072fd2a5db26a6f319280b389
Mandy Mason <mnmason86@gmail.com>
2022-07-26 01:36:23 -0400

    updated README

--- seed.js
+++ seed.js
@@ -10,17 +10,17 @@ async function seed(){
   await Book.create({
     title: 'Jurassic Park',
     description: 'A group of scientists visit an island where a visionary millionaire has created a theme park with actual dinosaurs and things go awry!',
-    hasRead: true, 
+    status: true, 
   });
   await Book.create({
     title: 'Ender\'s Game',
     description: 'The novel tells the story of a young boy, Ender Wiggin, who is sent to a training academy named Battle School, located in orbit above the Earth, built to train people to become soldiers that will one day battle against a vast alien race known as "Buggers"',
-    hasRead: true,
+    status: true,
   });
   await Book.create({
     title: 'Station Eleven',
     description: 'A post-apocalyptic thriller following a father and son making their away across a desolate world',
-    hasRead: false,
+    status: false,
   });
   mongoose.disconnect();
 };
